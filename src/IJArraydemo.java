c5205278ed79b965d8364775eed8e9dccb26c670
Sanskar Singhal <89839415+sanskar2109@users.noreply.github.com>
2022-01-11 16:35:19 +0530

    Initial commit

---
+++ src/IJArraydemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IJArraydemo {
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        Scanner s =new Scanner(System.in);
+
+        for (int i=0;i<arr.length;i++){
+            arr[i]=s.nextInt();
+        }
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
